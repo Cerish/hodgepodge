7e321afbce7f93c423d4c3075b5d25823f866ab5
lihj@hopesen.com.cn <731523780@qq.com>
2020-03-17 20:39:14 +0800

    commit vue springboot frame

---
+++ hodgepode/webserver/src/main/java/com/jack/WebserverApplication.java
@@ -0,0 +1,14 @@
+package com.jack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebserverApplication.class, args);
+        System.out.println("8888");
+    }
+
+}
